b097a5170750b2e012edc00be80cde63add37bf7
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-10-04 03:47:22 +0900

    Fix _archive

--- todo.py
+++ todo.py
@@ -147,10 +147,9 @@ def _archive(push=True):
             todos_archive.append(todo)
         else:
             todos_remain.append(todo)
-    content_archive = render_todo(date, todos_archive)
-
-    if not content_archive:
+    if not todos_archive:
         return
+    content_archive = render_todo(date, todos_archive)
 
     # archive
     archive_dir = osp.join(CACHE_DIR, date.strftime('%Y'))
